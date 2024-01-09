2950158b5d09f9921da0a2cbfec6f3339908ead6
Rene <racosta323@gmail.com>
2024-01-09 17:54:36 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective(parameter = "*"){
+    return function(adjective = "special"){
+        return `You are ${parameter}${adjective}${parameter}!`
+    };
+}
+
+const encouragingPromptFunction = wrapAdjective("||")("a dedicated programmer");
+console.log(encouragingPromptFunction);
